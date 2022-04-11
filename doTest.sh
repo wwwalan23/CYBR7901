fe26bb68f179eda4524a3e5077966e6b82f0d3b8
wwwalan23 <53963938+wwwalan23@users.noreply.github.com>
2022-04-11 20:55:17 +1000

    Split clustering
    Split the clustering method into group

--- doTest.sh
+++ doTest.sh
@@ -1,34 +1,29 @@
 #!/bin/sh
 
-echo "python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_10K.csv"
-      python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_10K.csv
+echo "python CYBR_Tlsh_can't_scale.py -f dataDir/mb_70K.csv"
+      python CYBR_Tlsh_can't_scale.py -f dataDir/mb_70K.csv
 echo
 
-echo "python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_25K.csv"
-      python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_25K.csv
+echo "python CYBR_Tlsh_can't_scale.py -f dataDir/mb_60K.csv"
+      python CYBR_Tlsh_can't_scale.py -f dataDir/mb_60K.csv
 echo
 
-echo "python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_50K.csv"
-      python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_50K.csv
+echo "python CYBR_Tlsh_can't_scale.py -f dataDir/mb_50K.csv"
+      python CYBR_Tlsh_can't_scale.py -f dataDir/mb_50K.csv
 echo
 
-echo "python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_100K.csv"
-      python CYBR7901_Evaluation_Result_Tlsh.py -f dataDir/mb_100K.csv
+echo "python CYBR_Tlsh_can't_scale.py -f dataDir/mb_40K.csv"
+      python CYBR_Tlsh_can't_scale.py -f dataDir/mb_40K.csv
 echo
 
-echo "python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_10K.csv"
-      python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_10K.csv
+echo "python CYBR_Tlsh_can't_cluster.py -f dataDir/mb_100K.csv"
+      python CYBR_Tlsh_can't_cluster.py -f dataDir/mb_100K.csv
 echo
 
-echo "python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_25K.csv"
-      python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_25K.csv
+echo "python CYBR_Tlsh_good.py -f dataDir/mb_390K.csv"
+      python CYBR_Tlsh_good.py -f dataDir/mb_390K.csv
 echo
 
-echo "python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_50K.csv"
-      python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_50K.csv
+echo "python CYBR_Ssdeep_goog.py -f dataDir/mb_390K.csv"
+      python CYBR_Ssdeep_goog.py -f dataDir/mb_390K.csv
 echo
-
-echo "python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_100K.csv"
-      python CYBR7901_Evaluation_Result_Ssdeep.py -f dataDir/mb_100K.csv
-echo
-
